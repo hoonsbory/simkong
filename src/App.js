d41125ab24fd86e7cc8176fd9591d6641487f522
unknown <tonymkcv@naver.com>
2020-05-21 21:11:55 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react"
 import { Link, Route, BrowserRouter as Router } from "react-router-dom"
-import Hello from "./hello"
-import About from "./About"
+// import Hello from "./hello"
+// import About from "./About"
 
 export default function App() {
   return (
     <Router>
+      <h1>메인페이지</h1>
       <header>
-        <a href="/test">
-          <button>Hello</button>
+        <Link to="/personalColor">
+          <button>퍼스널컬러 측정</button>
+        </Link> <br></br>
+        <a href="/personalColor">
+          <button>퍼스널컬러 측정2</button>
         </a>
-        <Link to="/about">
-          <button>About</button>
-        </Link>
       </header>
       <hr />
-        <Route exact path="/" component={Hello} />
-        <Route path="/about" component={About} />
+        {/* <Route exact path="/" component={Hello} />
+        <Route path="/about" component={About} /> */}
     </Router>
   )
 }
