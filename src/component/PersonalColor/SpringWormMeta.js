f77d5af5af0d3ef1f2cc756a59815b4a04eda4b9
unknown <tonymkcv@naver.com>
2020-05-26 01:16:10 +0900

    .

--- src/component/PersonalColor/SpringWormMeta.js
+++ src/component/PersonalColor/SpringWormMeta.js
@@ -3,7 +3,10 @@ import Meta from '../Meta'
 
 export default class SpringWormMeta extends Component {
     componentDidMount(){
-        window.location.href = "http://jaehoon-dayoff.ml.s3-website.ap-northeast-2.amazonaws.com/personalColor"
+        if(navigator.userAgent.indexOf("Mozilla") > -1){
+
+            window.location.href = "http://jaehoon-dayoff.ml.s3-website.ap-northeast-2.amazonaws.com/personalColor"
+        }
     }
     render() {
         const metaData = {
