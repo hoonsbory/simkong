d11b1de31fee8ff2725a4bc373dbed0f8ddea002
unknown <tonymkcv@naver.com>
2020-05-26 02:28:29 +0900

    .

--- src/component/PersonalColor/SpringWormMeta.js
+++ src/component/PersonalColor/SpringWormMeta.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 import Meta from '../Meta'
 
 export default class SpringWormMeta extends Component {
-    // componentDidMount(){
-    //     if(navigator.userAgent.indexOf("Mozilla") > -1){
+    componentDidMount(){
 
-    //         window.location.href = "http://jaehoon-dayoff.ml.s3-website.ap-northeast-2.amazonaws.com/personalColor"
-    //     }
-    // }
+            window.location.href = "http://jaehoon-dayoff.ml.s3-website.ap-northeast-2.amazonaws.com/personalColor"
+    }
     render() {
         const metaData = {
             title: "봄 웜톤",
