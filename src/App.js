032cd948de676e69389a12635af7614cba5baefa
unknown <tonymkcv@naver.com>
2020-05-26 17:01:37 +0900

    .

--- src/App.js
+++ src/App.js
@@ -21,10 +21,6 @@ export default function App() {
         <Link to="/summerCool">여름 쿨톤</Link>
         <Link to="/fallWorm">가을 웜톤</Link>
         <Link to="/winterCool">겨울 쿨톤</Link>
-        <Link to="/winterCoolMeta"></Link>
-        <Link to="/summerCoolMeta"></Link>
-        <Link to="/fallWormMeta"></Link>
-        <Link to="/springWormMeta"></Link>
         {/* <a href="/personalColor">
           <button>퍼스널컬러 측정</button>
 
