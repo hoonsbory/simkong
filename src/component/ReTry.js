a78130f9217bfe75c181cae65063704099900d87
unknown <tonymkcv@naver.com>
2020-05-31 04:20:02 +0900

    .

--- src/component/ReTry.js
+++ src/component/ReTry.js
@@ -6,7 +6,7 @@ export default class ReTry extends Component {
             background : "rgb(206,222,247)",
             borderRadius : "100px",
             color : "white",
-            fontSize : "1.5rem",
+            fontSize : "1.2rem",
             display : "inline",
             cursor : "pointer"
         }
@@ -19,7 +19,7 @@ export default class ReTry extends Component {
         }
         return (
             <div style={main}>
-                <p id="reTryP"> ※ 카메라의 특성상 실제 피부색을 100% 담을 수 없어 결과가 다소 부정확할 수 있습니다.</p>
+                <p id="reTryP"> ※ 카메라의 특성상 실제 피부색을 100% 담을 수 없으므로 진단결과가 다소 부정확할 수 있습니다.</p>
                 <div onClick={()=> window.location.href = "https://mycolor.kr"} className="px-5 pt-2" style={retry}>
                 다시 테스트하기
 
