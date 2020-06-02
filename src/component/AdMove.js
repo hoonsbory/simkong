3b11f455c634092611ccc754e6a70965c226beda
unknown <tonymkcv@naver.com>
2020-06-03 04:20:34 +0900

    .

--- src/component/AdMove.js
+++ src/component/AdMove.js
@@ -7,8 +7,9 @@ export default function AdMove() {
           var dd = cou.firstElementChild;
           if(dd.firstElementChild.height=="120"){
               document.getElementsByClassName("pcTopDiv")[0].parentNode.insertBefore(dd, document.getElementsByClassName("pcTopDiv")[0].nextSibling)
-              var daumAd1 = document.getElementById("root").nextSibling;
-              var daumAd2 = daumAd1.nextSibling;
+              var daumAd1 = document.getElementById("root").nextElementSibling;
+              var daumAd2 = daumAd1.nextElementSibling;
+              daumAd2.setAttribute("class",daumAd2.getAttribute("class")+" ads")
               document.getElementsByClassName("pcMainDiv")[0].insertBefore(daumAd1, document.getElementsByClassName("specLip")[1].parentNode)
               document.getElementById("root").firstChild.insertBefore(daumAd2, document.getElementById("disqus_thread"))
               document.querySelector("body").style.opacity = 1;
