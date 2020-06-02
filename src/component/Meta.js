3bf8dd37f3caadcb88b24f78539b4343db831b17
unknown <tonymkcv@naver.com>
2020-06-02 20:19:16 +0900

    .

--- src/component/Meta.js
+++ src/component/Meta.js
@@ -24,6 +24,7 @@ const Meta = ({ data }) => {
       {canonical ? <link rel="canonical" href={canonical} /> : null}
       {image ? <link rel="image_src" href={image} /> : null}
       {image ? <meta itemprop="image" content={image} /> : null}
+      <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js"  async></script>
       
 
       
