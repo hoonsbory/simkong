54f08ec51094bf75a038434c1cc4cc2aaa406c28
unknown <tonymkcv@naver.com>
2020-06-05 00:23:05 +0900

    .

--- src/component/AdMove.js
+++ src/component/AdMove.js
@@ -6,13 +6,20 @@ export default function AdMove() {
           cou.firstElementChild.remove()
           var dd = cou.firstElementChild;
           if(dd.firstElementChild.height=="120"){
-              document.getElementsByClassName("pcTopDiv")[0].parentNode.insertBefore(dd, document.getElementsByClassName("pcTopDiv")[0].nextSibling)
-              var daumAd1 = document.getElementById("root").nextElementSibling;
-              var daumAd2 = daumAd1.nextElementSibling;
-              daumAd2.setAttribute("class",daumAd2.getAttribute("class")+" ads")
-              document.getElementsByClassName("specLip")[0].parentNode.insertBefore(daumAd1, document.getElementsByClassName("specLip")[0].previousSibling)
-              document.getElementById("root").firstChild.insertBefore(daumAd2, document.getElementById("disqus_thread"))
-              document.querySelector("body").style.opacity = 1;
+              try {
+                  
+                  document.getElementsByClassName("pcTopDiv")[0].parentNode.insertBefore(dd, document.getElementsByClassName("pcTopDiv")[0].nextSibling)
+                  var daumAd1 = document.getElementById("root").nextElementSibling;
+                  var daumAd2 = daumAd1.nextElementSibling;
+                  daumAd2.setAttribute("class",daumAd2.getAttribute("class")+" ads")
+                  document.getElementsByClassName("specLip")[0].parentNode.insertBefore(daumAd1, document.getElementsByClassName("specLip")[0].previousSibling)
+                  document.getElementById("root").firstChild.insertBefore(daumAd2, document.getElementById("disqus_thread"))
+                  document.querySelector("body").style.opacity = 1;
+            } catch (error) {
+                document.querySelector("body").style.opacity = 1;
+            }
+          }else{
+            document.querySelector("body").style.opacity = 1;
           }
         }
     return (
