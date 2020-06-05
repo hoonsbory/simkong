e247fdce330532be3b2408d1e16a2bb2464cb559
unknown <tonymkcv@naver.com>
2020-06-05 09:01:50 +0900

    .

--- src/component/AdMove.js
+++ src/component/AdMove.js
@@ -6,7 +6,6 @@ export default function AdMove() {
           cou.firstElementChild.remove()
           var dd = cou.firstElementChild;
           if(dd.firstElementChild.height=="120"){
-              try {
                   
                   document.getElementsByClassName("pcTopDiv")[0].parentNode.insertBefore(dd, document.getElementsByClassName("pcTopDiv")[0].nextSibling)
                   var daumAd1 = document.getElementById("root").nextElementSibling;
@@ -14,13 +13,8 @@ export default function AdMove() {
                   daumAd2.setAttribute("class",daumAd2.getAttribute("class")+" ads")
                   document.getElementsByClassName("specLip")[0].parentNode.insertBefore(daumAd1, document.getElementsByClassName("specLip")[0].previousSibling)
                   document.getElementById("root").firstChild.insertBefore(daumAd2, document.getElementById("disqus_thread"))
-                  document.querySelector("body").style.opacity = 1;
-            } catch (error) {
+                }
                 document.querySelector("body").style.opacity = 1;
-            }
-          }else{
-            document.querySelector("body").style.opacity = 1;
-          }
         }
     return (
         <div>
