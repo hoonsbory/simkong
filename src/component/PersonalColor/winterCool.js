23a3e4e963b0a82d68febe9b8bc5b71cb15aafc4
unknown <tonymkcv@naver.com>
2020-06-10 12:57:08 +0900

    .

--- src/component/PersonalColor/winterCool.js
+++ src/component/PersonalColor/winterCool.js
@@ -18,7 +18,6 @@ export default class WinterCool extends Component {
         AdMove()
         var d = document, s = d.createElement('script');
     s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-    s.setAttribute('data-ad-client', 'ca-pub-9106671749645972');
     s.async = true;
         (d.head || d.body).appendChild(s);
         window.adsbygoogle = window.adsbygoogle || []
