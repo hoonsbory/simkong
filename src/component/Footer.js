b613cf71d580816192f9418449c53a3dbf550729
hoonsbory <tonymkcv@naver.com>
2020-07-10 14:44:49 +0900

    애드센스 삭제

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -16,7 +16,6 @@ const Footer = () => {
                     <img alt="마이퍼스널컬러 로고" style={{width : "8rem", marginBottom : ".5rem"}} src="/images/personalColor/footerImg.png" />
                     <p>mycolor.kr</p>
                 </div>
-                <div>제작 도움 - <a href="https://www.youtube.com/channel/UCQNE2JmbasNYbjGAcuBiRRg" style={{color : "rgb(10, 59, 112)"}}>JoCoding Youtube</a></div>
                 <div id="footerCenter"><a href="https://mycolor.kr/privacy" style={{color : "rgb(10, 59, 112)"}}>개인정보 처리방침</a></div>
                 <div id="footerRight">Contact : 신재훈 -tonymkcv93@gmail.com</div>
             </footer>
