13404282701ecea3984903e0969f2382ee69dd0d
hoonsbory <tonymkcv@naver.com>
2020-08-27 19:58:43 +0900

    디스커스 리소크 크기를 너무 잡아먹어서 뺴버림. 쿠팡도 폰트때문에 1메가 가량 잡아먹지만 살려둠

--- src/App.js
+++ src/App.js
@@ -15,10 +15,6 @@ const App = () => {
   window.scrollTo(0, 0)
   window.Kakao.init('7e83cda1ddcc964d3fb6c99c8fd47645');
 
-  var d = document, s = d.createElement('script');
-  s.src = 'https://personalcolor.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
 
   const retry = {
     background: "rgb(206,222,247)",
@@ -60,7 +56,6 @@ const App = () => {
         <Route exact path="/summerCoolMeta" component={SummerCoolMeta}></Route>
         <Route exact path="/fallWarmMeta" component={FallWarmMeta}></Route>
         <Route exact path="/winterCoolMeta" component={WinterCoolMeta}></Route>
-        <div id="disqus_thread" />
         <ins className="kakao_ad_area ads" style={{ display: "none" }} data-ad-unit="DAN-qhq4nydjh6b5" data-ad-width="320"
           data-ad-height="100"></ins>
         <ins className="kakao_ad_area" style={{display: "none"}}
