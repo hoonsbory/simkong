92afb9f4997dc974ee22c50b42e9977247ebdea0
hoonsbory <tonymkcv@naver.com>
2020-12-03 14:27:57 +0900

    푸터 변경

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -17,7 +17,8 @@ const Footer = () => {
                     <p>mycolor.kr</p>
                 </div>
                 <div id="footerCenter"><a href="https://mycolor.kr/privacy" style={{color : "rgb(10, 59, 112)"}}>개인정보 처리방침</a></div>
-                <div id="footerRight">Contact : 신재훈 -tonymkcv93@gmail.com</div>
+                <a href="https://www.youtube.com/channel/UCQNE2JmbasNYbjGAcuBiRRg" style="color : rgb(10, 59, 112)">제작 도움 - 조코딩 유튜브</a>
+                <div id="footerRight">Contact : 신재훈 - <a href="mailto:tonymkcv93@gmail.com">tonymkcv93@gmail.com</a></div>
             </footer>
     )
 }
